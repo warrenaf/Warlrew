ff5872e8ff8d1d4048938247e13f64cb93a23454
warrenaf <123114766+warrenaf@users.noreply.github.com>
2026-04-16 07:35:58 -0400

    Delete NavBar.js

--- NavBar.js
+++
@@ -1,6 +0,0 @@
-export default function renderNavbar(data) {
-  const header = document.querySelector("header");
-  header.innerHTML = `<nav class="navbar">
-      <a class="nav-link" href="./">Home</a>
-      </nav>`;
-}
